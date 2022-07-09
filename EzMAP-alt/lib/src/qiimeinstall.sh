2d763020b857355a36efd742ee8242a7a2794d39
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-07-09 16:21:52 +0900

    gives idempotence to the script

--- EzMAP-alt/lib/src/qiimeinstall.sh
+++ EzMAP-alt/lib/src/qiimeinstall.sh
@@ -1,2 +1,2 @@
-osascript -e 'tell app "Terminal" to do script "source activate qiime2-2020.2 &&
+osascript -e 'tell app "Terminal" to do script "conda remove -n qiime2-2020.2 --all &&
 bash ~/Desktop/EzMAP/scripts/installqiime.sh"'

82ab795b5c5d4c6271fee8ea2e077728f1fbecb3
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-07-09 17:34:53 +0900

    fix file name

--- EzMAP-alt/lib/src/debularExec.sh
+++ EzMAP-alt/lib/src/debularExec.sh
@@ -1,2 +1,2 @@
 osascript -e 'tell app "Terminal" to do script "source activate qiime2-2020.2 &&
-bash ~/Desktop/EzMAP/scripts/single-end-import_deblur.sh"'
+bash ~/Desktop/EzMAP/scripts/single_end_import_deblur.sh"'

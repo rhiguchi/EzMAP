190b32226c86a8b5e836ebc30cd50ea91d6a5232
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-07-09 16:46:19 +0900

    simply copied from EzMap.zip

---
+++ EzMAP-alt/scripts/single_end_unite_dada2.sh
@@ -0,0 +1,20 @@
+echo "##### EzMAP ########"
+echo "#### UNITE Taxonomy Classification  #########"
+
+cd ~/Desktop/EzMAP_Analysis/EzMAP_Single_End_Read_Analysis
+
+mkdir Taxonomy
+mkdir Taxonomy/UNITE
+mkdir Taxonomy/UNITE/qza/DADA2
+mkdir Taxonomy/UNITE/qzv/DADA2
+
+time qiime feature-classifier classify-sklearn \
+  --i-classifier ~/Desktop/EzMAP/src/db/UNITE-classifier.qza \
+  --i-reads DADA2/qza/single-end-rep-seqs-dada2.qza \
+  --o-classification Taxonomy/UNITE/qza/DADA2/single-end-UNITE-taxonomy-dada2.qza
+
+time qiime metadata tabulate \
+  --m-input-file Taxonomy/UNITE/qza/DADA2/single-end-UNITE-taxonomy-dada2.qza \
+  --o-visualization Taxonomy/UNITE/qzv/DADA2/single-end-UNITE-taxonomy-dada2.qzv
+
+echo "#### done ####"

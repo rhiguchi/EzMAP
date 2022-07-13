e84167d52e08804b88a16fedd2f019618bd89ec7
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-07-13 10:32:06 +0900

    copy from the original

---
+++ EzMAP-alt/scripts/installqiime.sh
@@ -0,0 +1,33 @@
+echo "########### EzMAP ##############"
+echo "#### Installing QIIME2 #########"
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+
+bash ~/miniconda.sh
+
+conda update conda
+
+conda install wget
+
+wget https://data.qiime2.org/distro/core/qiime2-2021.2-py36-osx-conda.yml
+
+conda env create -n qiime2-2020.2 --file qiime2-2021.2-py36-osx-conda.yml
+
+
+wget \
+  -O "gg-13-8-99-515-806-nb-classifier.qza" \
+  "https://data.qiime2.org/2021.2/common/gg-13-8-99-515-806-nb-classifier.qza"
+
+
+mv gg-13-8-99-515-806-nb-classifier.qza ~/Desktop/EzMAP/src/db/gg-13-8-99-515-806-nb-classifier.qza
+
+wget \
+  -O "silva-classifier.qza" \
+"https://data.qiime2.org/2020.11/common/silva-138-99-515-806-nb-classifier.qza"
+
+mv silva-classifier.qza ~/Desktop/EzMAP/src/db/silva-classifier.qza
+
+
+echo "####  Done #########"
+
+

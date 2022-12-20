d957014bc2291baaa40db84e0e2b034494f461ab
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-12-20 10:40:45 +0900

    modified to use EzMAP_config.txt

--- EzMAP-alt/scripts/pair_end_import.sh
+++ EzMAP-alt/scripts/pair_end_import.sh
@@ -6,10 +6,18 @@ cd ~/Desktop/EzMAP_Analysis
 mkdir EzMAP_Paired_End_Read_Analysis
 
 
+infile="$HOME/Desktop/EzMAP_config.txt"
+
+v2=$(head -n 1 "$infile")
+
+v3=$(sed -n '2{p;q;}' "$infile")
+
+echo "$v2"
+echo "$v3"
 
 time qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
-  --input-path Bacteria_manifest \
+  --input-path "$v3" \
   --output-path EzMAP_Paired_End_Read_Analysis/paired-end-demux.qza \
   --input-format PairedEndFastqManifestPhred64V2
 

cc5cfb60bff7c888ce1207f98ff1c6652797a618
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2022-12-20 10:46:01 +0900

    fixed to use Paired_primer_config.txt to primer options

--- EzMAP-alt/scripts/pair_end_cutadapter.sh
+++ EzMAP-alt/scripts/pair_end_cutadapter.sh
@@ -1,13 +1,19 @@
 echo "##### EzMAP ########"
 echo "### Single-end reads Cutadaptor in Execution ####"
 
-cd ~/Desktop/EzMAP_Analysis/EzMAP_Paired_End_Read_Analysis
+PRIMER_FILE="$HOME/Desktop/Paired_primer_config.txt"
+PRIMER_FRONT_FORWARD=$(head -n 1 "$PRIMER_FILE")
+PRIMER_FRONT_REVERSE=$(head -n 2 "$PRIMER_FILE" | tail -n 1)
 
+echo "PRIMER_FRONT_FORWARD: $PRIMER_FRONT_FORWARD"
+echo "PRIMER_FRONT_REVERSE: $PRIMER_FRONT_REVERSE"
+
+cd ~/Desktop/EzMAP_Analysis/EzMAP_Paired_End_Read_Analysis
 
 time qiime cutadapt trim-paired \
 --i-demultiplexed-sequences paired-end-demux.qza \
---p-front-f CCTACGGGNGGCWGCAG \
---p-front-r GACTACHVGGGTATCTAATCC \
+--p-front-f "$PRIMER_FRONT_FORWARD" \
+--p-front-r "$PRIMER_FRONT_REVERSE" \
 --o-trimmed-sequences paired-end-trimmed-demux.qza \
 --verbose > paired_end_cutadapt_log.txt
 

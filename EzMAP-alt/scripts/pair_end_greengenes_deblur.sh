3dabd3d043ad40ddfb1df4ad1229d3136c7ade67
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2023-01-14 16:22:20 +0900

    Extract common paths to a variable

--- EzMAP-alt/scripts/pair_end_greengenes_deblur.sh
+++ EzMAP-alt/scripts/pair_end_greengenes_deblur.sh
@@ -4,9 +4,10 @@ echo "#### Greengenes Taxonomy Classification  #########"
 cd ~/Desktop/EzMAP_Analysis/EzMAP_Paired_End_Read_Analysis
 
 QZA_OUTPUT="Taxonomy/Greengenes/qza/Deblur"
+QZV_OUTPUT="Taxonomy/Greengenes/qzv/Deblur"
 
 mkdir -p "$QZA_OUTPUT"
-mkdir -p Taxonomy/Greengenes/qzv/Deblur
+mkdir -p "$QZV_OUTPUT"
 
 time qiime feature-classifier classify-sklearn \
   --i-classifier ~/Desktop/EzMAP/src/db/gg-13-8-99-515-806-nb-classifier.qza \
@@ -15,7 +16,7 @@ time qiime feature-classifier classify-sklearn \
 
 time qiime metadata tabulate \
   --m-input-file "$QZA_OUTPUT/paired-end-greengenes-taxonomy-deblur.qza" \
-  --o-visualization Taxonomy/Greengenes/qzv/Deblur/paired-end-greengenes-taxonomy-deblur.qzv
+  --o-visualization "$QZV_OUTPUT/paired-end-greengenes-taxonomy-deblur.qzv"
 
 echo "#### done ####"
 

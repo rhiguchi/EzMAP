012cb44a3fcf502e26b66bbf11f0e3fb810681c0
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2023-01-14 16:07:24 +0900

    Extract common paths to a variable

--- EzMAP-alt/scripts/pair_end_greengenes_dada2.sh
+++ EzMAP-alt/scripts/pair_end_greengenes_dada2.sh
@@ -3,16 +3,18 @@ echo "#### Greengenes Taxonomy Classification  #########"
 
 cd ~/Desktop/EzMAP_Analysis/EzMAP_Paired_End_Read_Analysis
 
-mkdir -p Taxonomy/Greengenes/qza/DADA2
+QZA_OUTPUT="Taxonomy/Greengenes/qza/DADA2"
+
+mkdir -p "$QZA_OUTPUT"
 mkdir -p Taxonomy/Greengenes/qzv/DADA2
 
 time qiime feature-classifier classify-sklearn \
   --i-classifier ~/Desktop/EzMAP/src/db/gg-13-8-99-515-806-nb-classifier.qza \
   --i-reads DADA2/qza/paired-end-rep-seqs-dada2.qza \
-  --o-classification Taxonomy/Greengenes/qza/DADA2/paired-end-greengenes-taxonomy-dada2.qza
+  --o-classification "$QZA_OUTPUT/paired-end-greengenes-taxonomy-dada2.qza"
 
 time qiime metadata tabulate \
-  --m-input-file Taxonomy/Greengenes/qza/DADA2/paired-end-greengenes-taxonomy-dada2.qza \
+  --m-input-file "$QZA_OUTPUT/paired-end-greengenes-taxonomy-dada2.qza" \
   --o-visualization Taxonomy/Greengenes/qzv/DADA2/paired-end-greengenes-taxonomy-dada2.qzv
 
 
@@ -21,7 +23,7 @@ echo "#### Exporting ####"
 mkdir -p Taxonomy/Greengenes/qzv/DADA2/Export
 
 qiime tools export \
-  --input-path Taxonomy/Greengenes/qza/DADA2/paired-end-greengenes-taxonomy-dada2.qza \
+  --input-path "$QZA_OUTPUT/paired-end-greengenes-taxonomy-dada2.qza" \
   --output-path Taxonomy/Greengenes/qzv/DADA2/Export
 
 qiime tools export \

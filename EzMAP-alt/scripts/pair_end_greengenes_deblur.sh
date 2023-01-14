081eef551b824220a325134d0d1c5a3eae2b9a9e
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2023-01-14 16:21:34 +0900

    Extract common paths to a variable

--- EzMAP-alt/scripts/pair_end_greengenes_deblur.sh
+++ EzMAP-alt/scripts/pair_end_greengenes_deblur.sh
@@ -3,16 +3,18 @@ echo "#### Greengenes Taxonomy Classification  #########"
 
 cd ~/Desktop/EzMAP_Analysis/EzMAP_Paired_End_Read_Analysis
 
-mkdir -p Taxonomy/Greengenes/qza/Deblur
+QZA_OUTPUT="Taxonomy/Greengenes/qza/Deblur"
+
+mkdir -p "$QZA_OUTPUT"
 mkdir -p Taxonomy/Greengenes/qzv/Deblur
 
 time qiime feature-classifier classify-sklearn \
   --i-classifier ~/Desktop/EzMAP/src/db/gg-13-8-99-515-806-nb-classifier.qza \
   --i-reads Deblur/qza/paired-end-rep-seqs-deblur.qza \
-  --o-classification Taxonomy/Greengenes/qza/Deblur/paired-end-greengenes-taxonomy-deblur.qza
+  --o-classification "$QZA_OUTPUT/paired-end-greengenes-taxonomy-deblur.qza"
 
 time qiime metadata tabulate \
-  --m-input-file Taxonomy/Greengenes/qza/Deblur/paired-end-greengenes-taxonomy-deblur.qza \
+  --m-input-file "$QZA_OUTPUT/paired-end-greengenes-taxonomy-deblur.qza" \
   --o-visualization Taxonomy/Greengenes/qzv/Deblur/paired-end-greengenes-taxonomy-deblur.qzv
 
 echo "#### done ####"

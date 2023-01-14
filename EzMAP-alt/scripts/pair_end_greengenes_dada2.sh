bc75f0b4fe616234c8b387044077daaaa8372627
HIGUCHI Ryusuke <higuchi@sci-d.co.jp>
2023-01-14 15:57:37 +0900

    Added export process

--- EzMAP-alt/scripts/pair_end_greengenes_dada2.sh
+++ EzMAP-alt/scripts/pair_end_greengenes_dada2.sh
@@ -15,4 +15,33 @@ time qiime metadata tabulate \
   --m-input-file Taxonomy/Greengenes/qza/DADA2/paired-end-greengenes-taxonomy-dada2.qza \
   --o-visualization Taxonomy/Greengenes/qzv/DADA2/paired-end-greengenes-taxonomy-dada2.qzv
 
+
+echo "#### Exporting ####"
+
+mkdir -p Taxonomy/Greengenes/qzv/DADA2/Export
+
+qiime tools export \
+  --input-path Taxonomy/Greengenes/qza/DADA2/paired-end-greengenes-taxonomy-dada2.qza \
+  --output-path Taxonomy/Greengenes/qzv/DADA2/Export
+
+qiime tools export \
+  --input-path DADA2/qza/paired-end-table-dada2.qza \
+  --output-path Taxonomy/Greengenes/qzv/DADA2/Export
+
+biom add-metadata \
+  -i Taxonomy/Greengenes/qzv/DADA2/Export/feature-table.biom \
+  -o Taxonomy/Greengenes/qzv/DADA2/Export/feature-table-with-taxonomy.biom \
+  --observation-metadata-fp Taxonomy/Greengenes/qzv/DADA2/Export/taxonomy.tsv \
+  --observation-header OTUID,taxonomy \
+  --sc-separated taxonomy
+
+biom add-metadata \
+  -i Taxonomy/Greengenes/qzv/DADA2/Export/feature-table-with-taxonomy.biom \
+  -o Taxonomy/Greengenes/qzv/DADA2/Export/feature-table-with-taxonomy-meta.biom \
+  --sample-metadata-fp ~/Desktop/EzMAP_Analysis/sample-metadata.tsv \
+  --sample-header sample-id,sample-site,cultivar,compartment
+
+cp Taxonomy/Greengenes/qzv/DADA2/Export/feature-table-with-taxonomy-meta.biom table-w-tax-meta.biom
+
+
 echo "#### done ####"
